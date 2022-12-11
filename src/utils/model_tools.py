d89f8e73f6d7aa43fecd91f42ea2424aec197b3b
Maggie von Ebers <vonebersm@gmail.com>
2022-12-10 18:02:15 -0600

    initial setup for gfmnist

--- src/utils/model_tools.py
+++ src/utils/model_tools.py
@@ -112,7 +112,7 @@ def test(dataloader, model, loss_fn, device, swap=False, swap_labels=[], classes
     print(
         f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-    return test_loss, np.asarray(y_pred_list), np.asarray(targets)
+    return test_loss, np.asarray(y_pred_list), np.asarray(targets), 100*correct
 
 
 def add_output_nodes(ckpt:str, device, num_new_outputs:int=1, arch:str='linear') -> torch.nn.Module:
