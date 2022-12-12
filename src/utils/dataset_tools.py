16baa8a102f1a7bb013297ff2ef0354aa7ed5ec8
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2022-12-11 18:50:57 -0600

    added custom image dataset class

--- src/utils/dataset_tools.py
+++ src/utils/dataset_tools.py
@@ -1,11 +1,37 @@
 import numpy as np
+import pandas as pd
 import torch
+import os
 
 from copy import copy
 from torch import utils
+from torchvision.io import read_image
 
 from utils.feature_extractor import *
 
+
+
+class SyntheticImageDataset(utils.data.Dataset):
+    def __init__(self, annotations_file, class_file, img_dir, transform=None, target_transform=None):
+        class_df = pd.read_csv(annotations_file)
+        self.classes = class_df.values.tolist()
+        self.targets = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.targets.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.targets.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
 
 
 def create_index_arrays(dataset, indices=[]):
